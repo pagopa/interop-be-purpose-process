c17e59830ce0e0d2c5f400f53d03689b08b3f949
galales <25105748+galales@users.noreply.github.com>
2022-02-04 15:43:39 +0100

    pin-966: Fix api error handling

--- src/main/scala/it/pagopa/pdnd/interop/uservice/purposeprocess/api/converters/purposemanagement/ProblemConverter.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/purposeprocess/api/converters/purposemanagement/ProblemConverter.scala
@@ -1,12 +1,19 @@
 package it.pagopa.pdnd.interop.uservice.purposeprocess.api.converters.purposemanagement
 
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import it.pagopa.pdnd.interop.uservice.purposemanagement.client.model.{
   Problem => PurposeProblem,
   ProblemError => PurposeProblemError
 }
 import it.pagopa.pdnd.interop.uservice.purposeprocess.model.{Problem, ProblemError}
+import spray.json._
 
-object ProblemConverter {
+import scala.util.Try
+
+object ProblemConverter extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit def problemErrorFormat: RootJsonFormat[PurposeProblemError] = jsonFormat2(PurposeProblemError)
+  implicit def problemFormat: RootJsonFormat[PurposeProblem]           = jsonFormat5(PurposeProblem)
+
   def dependencyToApi(problem: PurposeProblem): Problem = Problem(
     `type` = problem.`type`,
     status = problem.status,
@@ -14,6 +21,17 @@ object ProblemConverter {
     detail = problem.detail,
     errors = problem.errors.map(problemErrorToApi)
   )
+
+  def fromString(body: String): Try[Problem] =
+    Try(body.parseJson.convertTo[PurposeProblem]).map(problem =>
+      Problem(
+        `type` = problem.`type`,
+        status = problem.status,
+        title = problem.title,
+        detail = problem.detail,
+        errors = problem.errors.map(problemErrorToApi)
+      )
+    )
 
   def problemErrorToApi(problemError: PurposeProblemError): ProblemError =
     ProblemError(code = problemError.code, detail = problemError.detail)
--- src/main/scala/it/pagopa/pdnd/interop/uservice/purposeprocess/api/impl/PurposeApiServiceImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/purposeprocess/api/impl/PurposeApiServiceImpl.scala
@@ -11,13 +11,10 @@ import it.pagopa.pdnd.interop.commons.utils.AkkaUtils.getBearer
 import it.pagopa.pdnd.interop.commons.utils.OpenapiUtils.parseArrayParameters
 import it.pagopa.pdnd.interop.commons.utils.TypeConversions._
 import it.pagopa.pdnd.interop.uservice.catalogmanagement.client.invoker.{ApiError => CatalogApiError}
-import it.pagopa.pdnd.interop.uservice.catalogmanagement.client.model.{Problem => CatalogProblem}
 import it.pagopa.pdnd.interop.uservice.partymanagement.client.invoker.{ApiError => PartyApiError}
-import it.pagopa.pdnd.interop.uservice.partymanagement.client.model.{Problem => PartyProblem}
 import it.pagopa.pdnd.interop.uservice.purposemanagement.client.invoker.{ApiError => PurposeApiError}
 import it.pagopa.pdnd.interop.uservice.purposemanagement.client.model.{
   ChangedBy,
-  Problem => PurposeProblem,
   PurposeVersionState => DepPurposeVersionState
 }
 import it.pagopa.pdnd.interop.uservice.purposemanagement.client.{model => PurposeManagementDependency}
@@ -270,21 +267,21 @@ final case class PurposeApiServiceImpl(
 
   def handleApiError(defaultProblem: Problem): PartialFunction[Try[_], StandardRoute] = {
     case Failure(err: CatalogApiError[_]) =>
-      val problem = err.responseContent.fold(defaultProblem) {
-        case problem: CatalogProblem => catalogmanagement.ProblemConverter.dependencyToApi(problem)
-        case _                       => defaultProblem
+      val problem = err.responseContent match {
+        case Some(body: String) => catalogmanagement.ProblemConverter.fromString(body).getOrElse(defaultProblem)
+        case _                  => defaultProblem
       }
       complete(problem.status, problem)
     case Failure(err: PartyApiError[_]) =>
-      val problem = err.responseContent.fold(defaultProblem) {
-        case problem: PartyProblem => partymanagement.ProblemConverter.dependencyToApi(problem)
-        case _                     => defaultProblem
+      val problem = err.responseContent match {
+        case Some(body: String) => partymanagement.ProblemConverter.fromString(body).getOrElse(defaultProblem)
+        case _                  => defaultProblem
       }
       complete(problem.status, problem)
     case Failure(err: PurposeApiError[_]) =>
-      val problem = err.responseContent.fold(defaultProblem) {
-        case problem: PurposeProblem => purposemanagement.ProblemConverter.dependencyToApi(problem)
-        case _                       => defaultProblem
+      val problem = err.responseContent match {
+        case Some(body: String) => purposemanagement.ProblemConverter.fromString(body).getOrElse(defaultProblem)
+        case _                  => defaultProblem
       }
       complete(problem.status, problem)
   }
