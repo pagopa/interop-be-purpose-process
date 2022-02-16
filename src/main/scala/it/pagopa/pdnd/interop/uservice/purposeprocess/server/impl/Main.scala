56a47369f31e8f335c9b70c2c3ff507a96c0321a
galales <25105748+galales@users.noreply.github.com>
2022-02-16 10:16:53 +0100

    pin-1099: Fix jwt audience check

--- src/main/scala/it/pagopa/pdnd/interop/uservice/purposeprocess/server/impl/Main.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/purposeprocess/server/impl/Main.scala
@@ -117,7 +117,7 @@ object Main
     jwtValidator = new DefaultJWTReader with PublicKeysHolder {
       var publicKeyset: Map[KID, SerializedKey] = keyset
       override protected val claimsVerifier: DefaultJWTClaimsVerifier[SecurityContext] =
-        getClaimsVerifier()
+        getClaimsVerifier(audience = ApplicationConfiguration.jwtAudience)
     }
   } yield (jwtValidator, fileManager)
 
