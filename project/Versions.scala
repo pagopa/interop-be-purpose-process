97dbb071620f923f1ad4edc34601c84124326b74
Scala Steward <scala-steward@pagopa.it>
2022-03-23 16:56:29 +0000

    Update akka-actor, ... to 2.6.19 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   lazy val akkaHttpVersion             = "10.2.6"
   lazy val akkaManagementVersion       = "1.1.1"
-  lazy val akkaVersion                 = "2.6.17"
+  lazy val akkaVersion                 = "2.6.19"
   lazy val cassandraPersistenceVersion = "1.0.5"
   lazy val catsVersion                 = "2.7.0"
   lazy val jacksonVersion              = "2.11.4"
