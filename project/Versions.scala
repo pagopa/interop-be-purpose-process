c91ad0e64f267c555fc747af80de424208de3a8e
Antonio Gelameris <toniogela89@gmail.com>
2022-06-15 19:58:02 +0200

    Revert "0.1.x: Bump to 1.0.0"
    This reverts commit 5e99db2ea0621c4eee43a07f35166a2d4d3c0a63.

--- project/Versions.scala
+++ project/Versions.scala
@@ -20,10 +20,10 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val agreementManagementVersion     = "1.0.0"
-  lazy val authorizationManagementVersion = "1.0.0"
-  lazy val purposeManagementVersion       = "1.0.0"
-  lazy val catalogManagementVersion       = "1.0.0"
-  lazy val partyManagementVersion         = "1.0.0"
-  lazy val commonsVersion                 = "1.0.0"
+  lazy val agreementManagementVersion     = "0.1.x-SNAPSHOT"
+  lazy val authorizationManagementVersion = "0.1.x-SNAPSHOT"
+  lazy val purposeManagementVersion       = "0.1.x-SNAPSHOT"
+  lazy val catalogManagementVersion       = "0.1.x-SNAPSHOT"
+  lazy val partyManagementVersion         = "0.1.x-SNAPSHOT"
+  lazy val commonsVersion                 = "0.1.x-SNAPSHOT"
 }
