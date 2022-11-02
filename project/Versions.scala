35d55a2dd9b59fccc1ceca9dea7537c66f3366ff
galales <25105748+galales@users.noreply.github.com>
2022-11-02 12:41:26 +0100

    Revert "1.0.x: Bump versions"
    This reverts commit 34dbbe42e2b824a3388c1ddf5ad3741dbd5578f6.

--- project/Versions.scala
+++ project/Versions.scala
@@ -19,11 +19,11 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val agreementManagementVersion     = "1.0.4"
-  lazy val authorizationManagementVersion = "1.0.6"
-  lazy val purposeManagementVersion       = "1.0.4"
-  lazy val catalogManagementVersion       = "1.0.5"
-  lazy val tenantManagementVersion        = "1.0.1"
-  lazy val partyManagementVersion         = "1.0.1"
-  lazy val commonsVersion                 = "1.0.8"
+  lazy val agreementManagementVersion     = "1.0.x-SNAPSHOT"
+  lazy val authorizationManagementVersion = "1.0.x-SNAPSHOT"
+  lazy val purposeManagementVersion       = "1.0.x-SNAPSHOT"
+  lazy val catalogManagementVersion       = "1.0.x-SNAPSHOT"
+  lazy val tenantManagementVersion        = "1.0.x-SNAPSHOT"
+  lazy val partyManagementVersion         = "1.0.x-SNAPSHOT"
+  lazy val commonsVersion                 = "1.0.x-SNAPSHOT"
 }
