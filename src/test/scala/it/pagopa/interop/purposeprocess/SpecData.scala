7635b53f1f58c4eb4c3a27f14920459b3db75281
Riccardo Torsoli <122275960+nttdata-rtorsoli@users.noreply.github.com>
2023-04-26 17:23:15 +0200

    PIN-2708 [BE] Log level a warning nel default error handler dei servizi per errori < 500 (#153)

--- src/test/scala/it/pagopa/interop/purposeprocess/SpecData.scala
+++ src/test/scala/it/pagopa/interop/purposeprocess/SpecData.scala
@@ -227,7 +227,8 @@ object SpecData {
       description = None,
       purposes = Seq.empty,
       relationships = Set.empty,
-      kind = AuthorizationManagement.ClientKind.CONSUMER
+      kind = AuthorizationManagement.ClientKind.CONSUMER,
+      createdAt = timestamp
     )
 
 }
